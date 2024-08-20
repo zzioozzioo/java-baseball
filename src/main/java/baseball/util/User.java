12618ea97faedbfa438ab07c25c0f15b750411aa
zzioozzioo <zzioo1006@naver.com>
2024-08-20 15:10:59 +0900

    refactor(Converter): rename from 'Format' to 'Converter'

--- src/main/java/baseball/util/User.java
+++ src/main/java/baseball/util/User.java
@@ -1,22 +1,21 @@
 package baseball.util;
 
-import baseball.validation.InputValidation;
+import baseball.validation.Validator;
 
 import java.util.List;
-
-import static baseball.util.Format.*;
-import static baseball.validation.InputValidation.*;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class User {
 
-    private final InputValidation validation = new InputValidation();
+    private final Validator validation = new Validator();
+    private final Converter converter = new Converter();
+
 
     public List<Integer> getPlayerNumbers(List<Integer> playerNumberList) {
         String userInput = readLine();
 
-        playerNumberList = playerNumberStringToList(userInput);
+        playerNumberList = converter.playerNumberToList(userInput);
         validation.validateStringThreeNumberDuplicate(playerNumberList);
 
         return playerNumberList;
