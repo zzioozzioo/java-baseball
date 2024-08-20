c485e4217dbeb447c38aa8dc6e98b5ad9a469fef
zzioozzioo <zzioo1006@naver.com>
2024-08-20 14:50:58 +0900

    refactor(BaseballGame): move the 'restartGame' method in the 'GameController' class to the 'BaseballGame' class

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import baseball.util.*;
+import baseball.validation.Validator;
 
 import static baseball.constant.ConstMessage.*;
 import static baseball.constant.ConstNumber.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
 
     private final Computer computer = new Computer();
     private final User user = new User();
     private final GameResult gameResult = new GameResult();
-    private final GameController gameController = new GameController();
+    private final Validator validation = new Validator();
+    private final Format format = new Format();
 
     public BaseballGame() {
         System.out.println(START_GAME_MESSAGE);
@@ -23,23 +26,21 @@ public class BaseballGame {
     public void startGame() {
 
         while (true) {
-
             // 게임 한 라운드 진행
             playOneRound();
 
             // 게임 재시작 여부 체크
-            if (!gameController.restartGame()) {
+            if (!restartGame()) {
                 return;
             }
         }
     }
 
     private void playOneRound() {
-        // 컴퓨터에서 랜덤 숫자 생성
+
         List<Integer> randomComputerNumberList = computer.getRandomComputerNumbers();
 
         while (true) {
-            // 사용자 랜덤 숫자 생성
             List<Integer> playerNumberList = new ArrayList<>(NUM_LENGTH);
 
             System.out.println(INPUT_NUMBER_MESSAGE);
@@ -54,6 +55,20 @@ public class BaseballGame {
                 break;
             }
         }
+    }
+
+    public boolean restartGame() {
+        System.out.println(CHOOSE_RESTART_OR_EXIT_MESSAGE);
+
+        String userInput = readLine();
+        validation.validateOneOrTwo(userInput);
+
+        int integerUserInput = format.toInt(userInput);
+
+        if (integerUserInput == RESTART_NUM) {
+            return true;
+        }
+        return false;
     }
 
 
