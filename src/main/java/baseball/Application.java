3ff61b7e9ca334d3054afc79b6721c78e1d814d3
zzioozzioo <zzioo1006@naver.com>
2024-08-21 17:16:14 +0900

    style(Application): clean up code formatting and remove unnecessary comments

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,25 +1,7 @@
 package baseball;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static baseball.util.Computer.*;
-import static baseball.util.GameResult.*;
-import static baseball.util.User.*;
-import static baseball.constant.ConstMessage.*;
-import static baseball.constant.ConstNumber.*;
-
-
-
 public class Application {
     public static void main(String[] args) {
-
-        // TODO: 코드를 성격별로 분류해야 하는데, 어떻게 설계하면 좋을까
-        // TODO: Application은 단지 Baseball 클래스의 객체를 생성, 호출해 게임을 시작하도록 하는 기능
-        //  Baseball에서 아래의 코드 실행하기
-        // TODO: 에러 메시지 더 상세하게 작성하기(나중에)
-
 
         BaseballGame baseballGame = new BaseballGame();
         baseballGame.startGame();
